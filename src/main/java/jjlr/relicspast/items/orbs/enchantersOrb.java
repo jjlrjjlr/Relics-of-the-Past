8460a6d9fb3cdca7b6c65249acff862cda5f197f
jjlr <mc.jjlr@tutanota.com>
2018-10-07 00:42:54 -0700

    Enchanters orb now removes enchantment from offhand item, enchantment is not stored in enchanters orb after.

--- src/main/java/jjlr/relicspast/items/orbs/enchantersOrb.java
+++ src/main/java/jjlr/relicspast/items/orbs/enchantersOrb.java
@@ -15,6 +15,10 @@ public class enchantersOrb extends basicItem {
 
 	public enchantersOrb() {
 		super("orb_enchanters", reference.modid);
+		
+		setMaxDamage(3);
+		setMaxStackSize(1);
+		
 	}
 	
 	@Override
@@ -27,14 +31,23 @@ public class enchantersOrb extends basicItem {
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		
 		ItemStack helditem = playerIn.getHeldItem(handIn);
-		
 		ItemStack offhand = playerIn.getHeldItemOffhand();
+		int randomseed = worldIn.rand.nextInt(100);
 		
 		if(offhand.isItemEnchanted() && worldIn.isRemote) {
 			
 			String taglist = offhand.getEnchantmentTagList().toString();
 			
+			/*Copy enchantment to helditem, then remove enchantment from offhand item*/
+			offhand.getEnchantmentTagList()
+			
 			playerIn.sendMessage(new TextComponentString(taglist));
+			
+			/*Attempt to break item*/
+			if(randomseed > 90) {
+				helditem.shrink(1);
+				playerIn.sendMessage(new TextComponentString("This Enchanters orb appears to have broken"));
+			}
 			
 			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, helditem);
 		} else if(worldIn.isRemote) {
