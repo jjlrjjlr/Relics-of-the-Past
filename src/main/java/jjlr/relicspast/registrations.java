af73ecd6e6922db0a8380a178e721dadfccb1573
jjlr <mc.jjlr@tutanota.com>
2018-10-06 22:38:50 -0700

    Removed inner class from registrations.java

--- src/main/java/jjlr/relicspast/registrations.java
+++ src/main/java/jjlr/relicspast/registrations.java
@@ -9,30 +9,28 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@EventBusSubscriber
 public class registrations {
 	
-	@EventBusSubscriber
-	public static class registrationHandler{
-		
-		/*Register blocks*/
-		@SubscribeEvent
-		public static void registerBlocks(RegistryEvent.Register<Block> event) {
-			modBlocks.register(event.getRegistry());
-		}
-		
-		/*Register Items*/
-		@SubscribeEvent
-		public static void registerItems(RegistryEvent.Register<Item> event) {
-			modBlocks.registerItemBlocks(event.getRegistry());
-			modItems.register(event.getRegistry());
-		}
-		
-		/*Register models*/
-		@SubscribeEvent
-		public static void registerModels(ModelRegistryEvent event) {
-			modBlocks.registerModels();
-			modItems.registerModels();
-		}
+	
+	/*Register blocks*/
+	@SubscribeEvent
+	public static void registerBlocks(RegistryEvent.Register<Block> event) {
+		modBlocks.register(event.getRegistry());
+	}
+	
+	/*Register Items*/
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		modBlocks.registerItemBlocks(event.getRegistry());
+		modItems.register(event.getRegistry());
+	}
+	
+	/*Register models*/
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event) {
+		modBlocks.registerModels();
+		modItems.registerModels();
 	}
 
 }
